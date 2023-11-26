fc9556199f9844ba47a86f2e013969ef663a553f
VALADI K JAGANATHAN <info@orchidsasia.com>
2023-11-26 14:34:36 +0530

    Update install.py

--- install.py
+++ install.py
@@ -2,17 +2,17 @@ import os, git, sys
 sys.path.insert(0,os.path.join(os.path.dirname(os.path.realpath(__file__)),"..",".."))
 
 def installer(custom_node_path):
-    repo_url = 'https://github.com/jags111/ComfyUI_jags_Vectormagic.git/'
-    repo_path = os.path.join(custom_node_path,"ComfyUI_jags_Vectormagic")
+    repo_url = 'https://github.com/jags111/ComfyUI_Jags_VectorMagic.git/'
+    repo_path = os.path.join(custom_node_path,"ComfyUI_Jags_VectorMagic")
     if os.path.exists(os.path.join(repo_path, '.git')):
-        print("Updating ComfyUI_jags_Vectormagic")
+        print("Updating ComfyUI_Jags_VectorMagic")
         repo:git.Repo = git.Repo(repo_path)
         origin = repo.remote(name='origin')
         origin.pull(rebase=True)
         repo.git.submodule('update','--init','--recursive')
         repo.close()
     else:
-        print("Installing ComfyUI_jags_Vectormagic")
+        print("Installing ComfyUI_Jags_VectorMagic")
         repo = git.Repo.clone_from(repo_url, repo_path)
         repo.git.clear_cache()
         repo.git.submodule('update','--init','--recursive')
